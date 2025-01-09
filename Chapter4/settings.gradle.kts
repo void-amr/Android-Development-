aaa5caf715be602d56581a9c545a307ffe2233a5
void-amr <aryanranjane9@gmail.com>
2025-01-09 21:32:29 +0530

    9/1/25 9:32pm

--- Chapter4/settings.gradle.kts
+++ Chapter4/settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "FirstMultiScreenApp"
 include(":app")
+include(":app")
  
---
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.explicitintent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editText = findViewById(R.id.editTextText);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Second_Activtiy.class);
+                intent.putExtra("message", editText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
